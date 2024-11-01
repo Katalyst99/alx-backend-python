94de27fcb596cd23fdea238466ba3163b75edf1b
Katalyst99 <katleho333@gmail.com>
2024-11-01 06:46:44 +0000

    2. Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""The module to parameterize a unit test"""
+"""The module for testing utils file"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,3 +25,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """Method to test that a KeyError is raised."""
         with self.assertRaises(output):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """The class that inherits from unittest.TestCase. for testing get_json"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ('http://holberton.io', {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Method to test that utils.get_json returns the expected result."""
+        mock = Mock()
+        mock.json.return_value = test_payload
+        with patch('requests.get', return_value=mock) as mock_get:
+            output = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(output, test_payload)
